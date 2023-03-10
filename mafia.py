5b9daf536e4b35d79397b66e5eb2ec0382f0401f
lazaniiia <124340097+lazaniiia@users.noreply.github.com>
2023-03-10 17:59:32 +1000

    Create mafia.py

---
+++ mafia.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+
+clicks = 0
+
+def click_button():
+    global clicks
+    clicks += 1
+    # изменяем текст на кнопке
+    btn["text"] = f"Clicks {clicks}"
+
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("250x200")
+
+# стандартная кнопка
+btn = ttk.Button(text="click me", command=click_button)
+btn.pack()
+root.mainloop()
